03ba42e30efeebf256a0e60ad77573ff721e1e17
Ingrid64f <ingridjaramillo848@gmail.com>
2025-08-13 00:05:25 -0500

    Ejercicios terminados

---
+++ app1.js
@@ -0,0 +1,56 @@
+/*
+
+let num= Number ( prompt ( "introduce un numero") )
+let counter = 1
+
+/// sumemos 1 a copunter 
+
+while (counter <= num) {
+  if( counter % 5 === 0 ) {
+console.log(counter)
+}
+counter++
+}*/
+
+
+//Ejercicio 2 (do while)
+//Primero ejecuta y luego evalúa
+/* do{
+  if( counter % 5 === 0 ){
+    console.log(counter)
+  }
+  counter++
+}
+while(counter <= num) */
+
+
+/*
+// Ejercicio 3 (for)
+
+for(let i = 1; i <= 50; i++) {
+  if( i % 2 != 0 ) {
+    console.log(i)
+  }
+} */
+
+  
+/* //Cree un ciclo for que imprima la tabla de multiplicara
+del 7 
+
+/* for(let i = 0; i <= 10; i++) { 
+console.log(,7 x ${i } = ${7*1} )
+console.log("7 x " + i +" = " + (7*1) )
+} */ 
+
+
+/*
+//Ejecicio 5
+//Cree un ciclo for que imprima 100 numeros
+
+for(let i = 1; i <= 100; i++) {
+  if( i >= 20 && i <= 60) {
+    continue
+  }
+  console.log(i)
+}
+*/
